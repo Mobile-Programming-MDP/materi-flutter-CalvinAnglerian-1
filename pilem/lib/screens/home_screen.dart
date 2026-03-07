43e31fe6fd8a793a1d4f76ea9b5ff58d98773998
CalvinAnglerian <calvinangelriansusanto24@gmail.com>
2026-03-07 15:07:35 +0700

    pertemuan5

--- pilem/lib/screens/home_screen.dart
+++ pilem/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
+import 'package:pilem/screens/detail_screen.dart';
 import 'package:pilem/services/api_services.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +12,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final ApiService _apiService = ApiService();
+
+  //Test
+  int _selectedIndex = 0;
 
   List<Movie> _allMovies = [];
   List<Movie> _trendingMovies = [];
@@ -24,9 +28,16 @@ class _HomeScreenState extends State<HomeScreen> {
     final List<Map<String, dynamic>> popularMoviesData = await _apiService.getPopularMovies();
 
     setState(() {
-      _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
-      _trendingMovies = trendingMoviesData.map((e) => Movie.fromJson(e)).toList();
-      _popularMovies = popularMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _allMovies = allMoviesData.map((json) => Movie.fromJson(json)).toList();
+      _trendingMovies = trendingMoviesData.map((json) => Movie.fromJson(json)).toList();
+      _popularMovies = popularMoviesData.map((json) => Movie.fromJson(json)).toList();
+    });
+  }
+
+  // Test
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;       
     });
   }
 
@@ -52,12 +63,33 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
+
+      // Test
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite'
+          ),
+        ],
+      ),
     );
   }
   Widget _buildMoviesList(String title, List<Movie> movies) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        // Menampilkan Title Kategori Movies
         Padding(
           padding: EdgeInsets.all(8.0),
           child: Text(
@@ -65,6 +97,7 @@ class _HomeScreenState extends State<HomeScreen> {
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
+        // Menampilkan thumbnail dan judul movies
         SizedBox(
           height: 200,
           child: ListView.builder(
@@ -72,23 +105,32 @@ class _HomeScreenState extends State<HomeScreen> {
             itemCount: movies.length,
             itemBuilder: (BuildContext context, int index) {
               final Movie movie = movies[index];
-              return Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Image.network('https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                    height: 150,
-                    width: 100,
-                    fit: BoxFit.cover,
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      movie.title,
-                      style: TextStyle(
-                        fontWeight:FontWeight.bold,
+              return GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(movie: movie),
+                  ), 
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Image.network('https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                      height: 150,
+                      width: 100,
+                      fit: BoxFit.cover,
                       ),
-                    )
-                  ],
+                      Text(
+                        movie.title.length > 14
+                          ? '${movie.title.substring(0, 10)}...'
+                          : movie.title,
+                        style: TextStyle(
+                          fontWeight:FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               );
             }
